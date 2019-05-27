609f5fa11fc0a855d1ceee20add1949a9cdb1e0a
Zane-ZZE <zane.zelenina@gmail.com>
2019-05-27 18:05:20 +0300

    Ready for mobile

--- class_work/scripts/first.js
+++ class_work/scripts/first.js
@@ -1,1 +1,29 @@
-alert("Hello World!");
+// alert("Hello World!");
+
+// document.body.style.backgroundColor = "green";
+
+// document.body.clientWidth 
+// client device width at the moment
+
+// check if equals or larger tahn 600px then green, else orange
+
+// FUNCTION
+
+// function changeColor(arg) {
+//     if (arg >= 600){
+//         document.body.style.backgroundColor = "green";
+//     } else {
+//         document.body.style.backgroundColor = "orange";
+//     }
+//     return arg;
+// }
+// changeColor(document.body.clientWidth);
+
+// WITHOUT FUNCTION
+var arg = document.body.clientWidth;
+
+if (arg > 600){
+    document.body.style.backgroundColor = "green";
+} else {
+    document.body.style.backgroundColor = "orange";
+}
